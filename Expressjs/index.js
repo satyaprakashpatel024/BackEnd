133a5eccbe6b1f33270390a1e109ecdb6a44e7b9
Satya Prakash Patel <satyaprakashpatel61@gmail.com>
2023-12-25 17:55:12 +0530

    changes commited

--- Expressjs/index.js
+++ Expressjs/index.js
@@ -3,9 +3,10 @@ let express = require('express');
 
 let app = express();
 
-app.use((req,res)=>{
-    res.send("Hello : localhost server.");
-})
+// app.use((req,res)=>{
+//     res.send("<h2>hello local Server</h2>");
+// })
+
 
 app.listen(3000,()=>{
     console.log("Server is running at http://localhost:3000");
