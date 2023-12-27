457459e04a56c49311413c36824ccc58720e14da
Satya Prakash Patel <satyaprakashpatel61@gmail.com>
2023-12-27 18:07:20 +0530

    changes saved

--- Expressjs/index.js
+++ Expressjs/index.js
@@ -2,12 +2,50 @@
 let express = require('express');
 
 let app = express();
+app.set('view engine','ejs');
 
 // app.use((req,res)=>{
 //     res.send("<h2>hello local Server</h2>");
 // })
 
 
+                            // static routing 
+
+
+app.get('/',(req,res)=>{
+    let random = Math.random()*100;
+    res.render('home',{random});
+    // res.send('<h2>this is home page</h2>');
+})
+
+// app.get('/new',(req,res)=>{
+//     res.send('<h2>this is new page</h2>');
+// })
+
+// app.get('/cat',(req,res)=>{
+//     res.send('<h2> mewwwww</h2>');
+// })
+
+// app.get('/*',(req,res)=>{
+//     res.send('<h2>galat hai</h2>');
+// })
+
 app.listen(3000,()=>{
     console.log("Server is running at http://localhost:3000");
 })
+
+                    // dynamic routing
+
+// app.get('/:key',(req,res)=>{
+//     let {key} = req.params;
+//     res.send(`<h2>${key}</h2>`);
+// })
+
+
+// querry parameter format : search(pathname)?firstName="value"&lastName="value"
+
+// app.get('/search',(req,res)=>{
+//     let {firstName,lastName} = req.query;
+//     // console.log(req.query);
+//     res.send(`${firstName} ${lastName}`)
+// })
