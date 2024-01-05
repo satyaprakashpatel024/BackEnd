678206c2cc5c1f2ebfc8046ebad519400c9a95af
Satya Prakash Patel <satyaprakashpatel61@gmail.com>
2024-01-06 00:35:42 +0530

    changes saved

--- Csr_ssr/index.js
+++ Csr_ssr/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
-
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Hello World!</h1>");
+app.use('/cat',express.static('public'))
+
+let arr = [1,2,3,4,5,'hello','hiii'];
+app.get('/todo',(req,res)=>{
+    res.json(arr);
 })
 
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello serevr !!!!!!!!!</h1>");
+})
 
 app.get('/home',(req,res)=>{
     res.send("<h1>Hello home page</h1>");
 })
 
-const port = 3000;
+app.post('/todo',(req,res)=>{
+    let {value} = req.body;
+    arr.push(value);
+});
+
+const port = 3001;
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ Csr_ssr/public/dom.js
@@ -0,0 +1,18 @@
+function send() {
+	$.get("/todo", (data) => {
+		console.log(data);
+		for (let item of data) {
+			$("#list").append(`<li>${item}</li>`);
+		}
+	});
+}
+
+send();
+
+$("#btn").on("click", () => {
+	let value = $("#inp").val();
+	// $("#inp").val() = '';
+	console.log(value);
+
+	$.post("/todo", { value }, () => {});
+});
