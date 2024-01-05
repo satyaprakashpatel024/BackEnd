2d4d0c7a05eb395903d0e7e391eb47db1f938378
Satya Prakash Patel <satyaprakashpatel61@gmail.com>
2024-01-05 17:56:40 +0530

    changes saved

--- Csr_ssr/index.js
+++ Csr_ssr/index.js
@@ -6,6 +6,11 @@ app.get('/',(req,res)=>{
     res.send("<h1>Hello World!</h1>");
 })
 
+
+app.get('/home',(req,res)=>{
+    res.send("<h1>Hello home page</h1>");
+})
+
 const port = 3000;
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
