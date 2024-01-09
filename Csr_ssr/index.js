ef4be9f204c99fe9339dadf3340c8760140e0c65
Satya Prakash Patel <satyaprakashpatel61@gmail.com>
2024-01-10 02:02:49 +0530

    changes saved

--- Csr_ssr/index.js
+++ Csr_ssr/index.js
@@ -17,29 +17,22 @@ app.get('/todo',(req,res)=>{
     }
 })
 
+// home page
 app.get('/',(req,res)=>{
     res.send("<h1>Hello serevr !!!!!!!!!</h1>");
 })
 
+// data receiving to update in array
 app.post('/todo',(req,res)=>{
     let {value} = req.body;
+    // console.log(value);
     arr.push(value);
 });
 
-// post request
-app.post('/post',(req,res)=>{
-    res.send(req.body);
-})
-
-// get request
-app.get('/get', (req, res) => {
-    res.send("get request");
-})
-
 // server side rendering file: todo
-app.get('/todo',(req,res)=>{
-    res.render('todo',{arr});
-})
+// app.get('/todo',(req,res)=>{
+//     res.render('todo',{arr});
+// })
 
 const port = 3001;
 app.listen(port,()=>{
